25f8fa5484b062ccc1129e090dd31339e93d2bf6
MatrixSystemPVP <kivikplay2003@gmail.com>
2021-07-24 20:21:26 +0200

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'MatrixCord',
+  description: 'Multipurpose framework for discord.js',
+  base: '/test/',
+  themeConfig: {
+    nav: [
+      { text: 'Documentation', link: '/documentation' },
+      { text: 'Github', link: 'https://github.com/MatrixSystemPVP/MatrixCord' },
+      { text: 'Discord', link: 'https://discord.gg/kEbQyy6' }
+    ]
+  }
+}
